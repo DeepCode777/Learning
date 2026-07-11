0528996a7b2a4eeb8e3611004c22bac209ff726c
Code With Deep <deeppoptani777@gmail.com>
2026-07-11 20:00:38 +0530

    Create script.js

---
+++ JavaScript/DOM/Question Part - 1/script.js
@@ -0,0 +1,37 @@
+/* 
+// Q - 1]
+let heading = document.querySelector("h2");
+console.dir(heading);
+
+heading.innerText += " From Deep poptani"; // concateString
+*/
+
+// Q - 2]
+// let divs = document.querySelectorAll(".box");
+// console.dir(divs);
+
+/*
+divs[0].innerText = "New conetnet 1";
+divs[1].innerText = "New conetnet 2";
+divs[2].innerText = "New conetnet 3";
+*/
+
+/*
+let i=0;
+for(div of divs){
+  div.innerText = `Hello from new div ${i}`;
+  i++;
+}
+*/
+
+/*
+for(let i=0; i<3; i++){
+  divs[i].innerText = (i+1)+" Div";
+}
+*/
+
+/*
+for(let i=0; i<3; i++){
+  divs[i].innerText = ` Hello Div ${i+1}`;
+}
+*/

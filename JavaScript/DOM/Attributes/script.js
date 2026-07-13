a307e08ad441e58179f1da421febc1dfff7ee50f
Code With Deep <deeppoptani777@gmail.com>
2026-07-13 09:46:24 +0530

    Create script.js

---
+++ JavaScript/DOM/Attributes/script.js
@@ -0,0 +1,57 @@
+/*
+DOM Manipulation
+ATTRIBUTES :- class id Name
+
+1] getAttribute(attr)
+      // to get attribute value
+      
+2] setAttribute(attr, value)
+      // ro set attribute value
+*/
+
+// let heading = document.querySelector("h4");
+// console.dir(heading);
+
+// let idH4 = heading.getAttribute("id");
+// console.log(idH4); // line 
+
+
+/*
+somthing wrong in ex for my pov
+
+let divs = document.querySelector("div");
+console.dir(divs);
+console.log(divs.getAttribute("class"));
+console.log(divs.getAttribute("id"));
+console.log(divs.getAttribute("name"));
+*/
+
+/*
+// in Elements
+let para = document.querySelector("p");
+console.log(para.getAttribute("class"));
+console.log(para.setAttribute("class","newClass"));
+
+////////////////////////////
+////      Style         ////
+////////////////////////////
+
+// => node.style : get style of node
+
+let divs = document.querySelector(".BOX");
+// console.log(divs);
+// console.log(divs.style);
+// divs.style.backgroundColor = "green";
+// divs.style.fontSize = "15px";
+// divs.innerText = "Hello...!";
+*/
+
+/*
+// Experiment
+let divs = document.querySelector("h4");
+console.log(divs);
+console.log(divs.style);
+divs.style.backgroundColor = "green";
+divs.style.fontSize = "15px";
+divs.innerText = "Hello...!";
+*/

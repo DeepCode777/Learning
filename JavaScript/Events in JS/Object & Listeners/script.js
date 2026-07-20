78c2c1b59831531eb0592733ddb9fac58ee4978f
Code With Deep <deeppoptani777@gmail.com>
2026-07-20 23:18:17 +0530

    Update script.js

--- JavaScript/Events in JS/Object & Listeners/script.js
+++ JavaScript/Events in JS/Object & Listeners/script.js
@@ -25,3 +25,37 @@ btn.ondblclick = (e) => {
   console.log(e.y);       //
 }
 */
+
+//////////////////////////////////
+////      Evenet Listener     ////
+//////////////////////////////////
+
+// node.addEventListener(event, callback);  //create new memory for add
+// node.removeEventListener(event, callback);
+
+// NOTE : the callback reference should be same to remove
+/*
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("click", () => {
+  console.log("This From Add Event Listener");
+});
+
+btn.addEventListener("click", (e) => {
+  console.log("Event Listener Objects : ",e);
+  console.log(e.type);
+  console.log(e.target);
+});*/
+
+
+// NOTE : the callback reference should be same to remove
+// So we pass callback function as variable when we remove same function
+/*
+let btn = document.querySelector("#btn");
+const ev = () => {
+  console.log("Event")
+};
+
+btn.addEventListener("click", ev);    //mouseover
+btn.removeEventListener("click",ev);  //dblclick
+*/

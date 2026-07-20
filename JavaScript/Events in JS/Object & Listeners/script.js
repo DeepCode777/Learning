4fccaca8255fbd80edc4c61f48e75c6f70bfa591
Code With Deep <deeppoptani777@gmail.com>
2026-07-20 23:16:57 +0530

    Create script.js

---
+++ JavaScript/Events in JS/Object & Listeners/script.js
@@ -0,0 +1,27 @@
+/*
+Event
+node.event = () => {
+  handling
+}
+
+
+event object
+node.event = (e) => {
+handling
+}
+*/
+/*
+let btn = document.querySelector("#dbl");
+
+btn.ondblclick = (e) => {
+  console.log(e);         // event object values on console
+  console.log(e.type);    //
+  console.log(e.target);  // element
+  console.log(e.clientX); //
+  console.log(e.clientY); //
+  console.log(e.screenX); //
+  console.log(e.screenY); //
+  console.log(e.x);       //
+  console.log(e.y);       //
+}
+*/

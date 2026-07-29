506e1d89ae0765629bfa14b3542f6d07ffd11c6f
Code With Deep <deeppoptani777@gmail.com>
2026-07-29 22:37:46 +0530

    Create urlRedirect.java

---
+++ Advance Java/Servlet/URL Redirect/urlRedirect.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class urlRedirect extends HttpServlet {
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws IOException, ServletException {
+
+        res.setContentType("text/html");
+
+        PrintWriter pw = res.getWriter();
+
+        pw.print("<h2><i><u>URL Redirect</u></i></h2>");
+        pw.print("<br>");
+
+        String nm = req.getParameter("name");
+        Int first = Integer.parseInt(req.getParameter("fnum"));
+        Int sec = Integer.parseInt(req.getParameter("snum"));
+        
+        int sum = first+sec;
+
+        pw.print("Name : " +nm);
+        pw.print("Sum of number : " +sum);
+
+
+        req.sendRedirect("sqr?key=" +sum);
+
+    }
+}

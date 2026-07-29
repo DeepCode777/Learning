2d27ee58d5de2e4105bb02d8113d6b764e60cbd8
Code With Deep <deeppoptani777@gmail.com>
2026-07-29 22:40:58 +0530

    Update urlRedirect.java

--- Advance Java/Servlet/URL Redirect/urlRedirect.java
+++ Advance Java/Servlet/URL Redirect/urlRedirect.java
@@ -4,11 +4,9 @@ import javax.servlet.http.*;
 
 public class urlRedirect extends HttpServlet {
 
-    public void doPost(HttpServletRequest req, HttpServletResponse res)
-            throws IOException, ServletException {
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 
         res.setContentType("text/html");
-
         PrintWriter pw = res.getWriter();
 
         pw.print("<h2><i><u>URL Redirect</u></i></h2>");
@@ -23,7 +21,7 @@ public class urlRedirect extends HttpServlet {
         pw.print("Name : " +nm);
         pw.print("Sum of number : " +sum);
 
-
+        // URL Redirect - sum redirect to sqr file
         req.sendRedirect("sqr?key=" +sum);
 
     }

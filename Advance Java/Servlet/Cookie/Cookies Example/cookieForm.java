fa56da7f41b664ca5557948d45d8dcb98a7c13e9
Code With Deep <deeppoptani777@gmail.com>
2026-07-30 22:45:56 +0530

    Update cookieForm.java

--- Advance Java/Servlet/Cookie/Cookies Example/cookieForm.java
+++ Advance Java/Servlet/Cookie/Cookies Example/cookieForm.java
@@ -5,12 +5,15 @@ import javax.servlet.http.*;
 public class cookieForm extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // create cookie - when requeste come
         Cookie firstName = new Cookie("first_name", request.getParameter("first_name"));
         Cookie lastName = new Cookie("last_name", request.getParameter("last_name"));
 
+        // set cookies valid duration(age - 2 minit)
         firstName.setMexAge(120);
         lastName.setMexAge(120);
 
+        // add cookies with response 
         response.addCookie(firstName);
         response.addCookie(lastName);
 

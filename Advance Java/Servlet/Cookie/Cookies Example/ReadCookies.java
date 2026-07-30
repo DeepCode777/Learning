81780224a3a4b7224a0596807d5dc20069140c93
Code With Deep <deeppoptani777@gmail.com>
2026-07-30 22:42:18 +0530

    Update ReadCookies.java

--- Advance Java/Servlet/Cookie/Cookies Example/ReadCookies.java
+++ Advance Java/Servlet/Cookie/Cookies Example/ReadCookies.java
@@ -2,12 +2,15 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class cookieForm extends HttpServlet {
+public class ReadCookies extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // initialisation var
         Cookie c = null;
+        // initialisation array for cookies
         Cookie[] cookies = null;
 
+        // get the cookie
         cookies = request.getCookies();
 
 
@@ -16,11 +19,11 @@ public class cookieForm extends HttpServlet {
         PrintWriter out = response.getWriter();
 
 
-
+        // check cookies is exist or not
         if( cookies != null){
             out.print("<h2> Found Cookies Name & value </h2>");
             for(int i=0; i<cookies.length; i++){
-                c = cookies[i];
+                c = cookies[i];    // cookies retrieve as array - can have multiple cookies at same time
                 out.print("Name : "+cookie.getName()+ " ");
                 out.print("Value : "+cookie.getValue()+ " </br>");
             }

bf4e295cb8824059130a665067f398b1587b7378
Code With Deep <deeppoptani777@gmail.com>
2026-07-30 23:00:35 +0530

    Update cookiesEx.java

--- Advance Java/Servlet/Cookie/cookiesEx.java
+++ Advance Java/Servlet/Cookie/cookiesEx.java
@@ -2,9 +2,10 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+@WebSwervlet("/cookieData")
 public class CookiesEx extends HttpServlet {
 
-    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 
         String n1 = req.getParameter("name");
         //Cookie cookie = new Cookie("key", "123");
@@ -12,26 +13,31 @@ public class CookiesEx extends HttpServlet {
         boolean uvisit = false;
 
         Cookie[] cookies = req.getCookies();
-        
+
+        // cookies is null or not
         if(cookies != null){
+            // user have cookies (revisit)
             for(int i=0; i<cookies.length; i++){
                 Cookie c = cookies[i];
                 String name = c.getName();
                 String val = c.getValue();
-                
+
+                // identify cookie
                 if(name.equals("k") && val.equal("123")){
                     out.print("Welcome Back " +n1);
-                    uvisit = true;
+                    uvisit = true;  // revisited user
                     break;
                 }
             }
         }
-
+        // user come first time 
         if(uvisit == false){
-            
+
+            // create cookies for user - because they have no cookies 
             Cookie c1 = new Cookie("key", "123");
+            // add cookies with response
             res.addCookies(c1);
-            out.print("Welcome " +n1);
+            out.print("Welcome " +n1);  // message for user
         }
     }
 }
